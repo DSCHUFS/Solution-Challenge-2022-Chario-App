169a935cc3d6418a0ebfe80091228d05fffcd55b
조성현/Cho_sung_hyun <37647483+chosunghyun18@users.noreply.github.com>
2022-02-09 11:11:51 +0900

    Delete App directory

--- App/lib/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const IconData person_rounded = IconData(0xf0071, fontFamily: 'MaterialIcons');
-const tcolor = const Color(0xff3eb489); // Second `const` is optional in assignment
-
-//#3EB489
