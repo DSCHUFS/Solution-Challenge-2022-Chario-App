e6d4697fa35c05bd77434db8ff55e03ec131e349
조성현/Cho_sung_hyun <37647483+chosunghyun18@users.noreply.github.com>
2022-02-20 16:29:48 +0900

    Update Userapi.dart
    upload change name of get variable 'data' to datag

--- App/lib/api/Userapi.dart
+++ App/lib/api/Userapi.dart
@@ -19,52 +19,62 @@ Future<UserJdata> fetchUserJdata() async {
   {
     String jsonData = response.body;
 
-    var myJson = jsonDecode(jsonData)['data']['u_username'];
-    print("user api check");
-    print(myJson);
+    var myJson = jsonDecode(jsonData);
 
-    return UserJdata.fromJson(jsonDecode(jsonData));
+    print(myJson);
+    print(myJson['data']);
+
+    //return myJson;
+    return UserJdata.fromJson(myJson);
   } else {
     throw Exception('Failed to load data');
   }
 }
 
 
-class UserJdata {
 
-  Data data;
+class UserJdata
+{
+
+  Data datag;
 
   UserJdata({
-    required this.data
+    required this.datag
   });
 
 
-  factory UserJdata.fromJson(Map<String, dynamic> parsedJson) {
+  factory UserJdata.fromJson(Map<String, dynamic> json)
+  {
     return UserJdata(
-        data: Data.fromJson(parsedJson['data'])
+        datag: Data.fromJson(json['data'])
     );
 
   }
 
 }
+
+
+
 class Data {
-  final String u_username;
-  final String u_name;
-  final String u_email;
-  final String u_phone;
-  final String u_birth;
+
+  final String u_Username;
+  final String u_Name;
+  final String u_Email;
+  final String u_Phone;
+  final String u_Birth;
 
 
-  Data({required this.u_username,required this.u_name, required this.u_email,required this.u_phone,required this.u_birth});
+  Data({required this.u_Username,required this.u_Name, required this.u_Email,required this.u_Phone,required this.u_Birth});
 
   factory Data.fromJson(Map<String, dynamic> json)
   {
-    return Data(u_username:
-        json["u_username"],
-        u_name: json["u_name]"],
-        u_email: json["u_email"],
-        u_phone: json["u_phone"],
-        u_birth: json["u_birth"]);
+    return Data(
+        u_Username: json["u_username"],
+        u_Name: json["u_name"],
+        u_Email: json["u_email"],
+        u_Phone: json["u_phone"],
+        u_Birth: json["u_birth"]
+    );
   }
 }
 
