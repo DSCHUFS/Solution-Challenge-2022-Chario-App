22d90186d988e82f81aa85887c37db71c9be9fef
조성현/Cho_sung_hyun <37647483+chosunghyun18@users.noreply.github.com>
2022-02-23 21:55:30 +0900

    Update subscribe_screen.dart
    update screen code for list view and list tile 
    problem : access problem in logo image url

--- App/lib/page1/subscribe_screen.dart
+++ App/lib/page1/subscribe_screen.dart
@@ -1,25 +1,23 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
-import 'Subscribeapi.dart';
-class SubscribeScreen extends StatefulWidget {
+import '../api/Subscribeapi.dart';
+
+
+class SubscribeScreen extends StatefulWidget
+{
   static const String id = "Subscribe_screen";
   @override
   _SubscribeScreenState createState() => _SubscribeScreenState();
 }
 
-class _SubscribeScreenState extends State<SubscribeScreen> {
-
+class _SubscribeScreenState extends State<SubscribeScreen>
+{
   late Future <DataList> Fcinform;
-
   @override
   void initState()
   {
     super.initState();
     Fcinform  = fetchSubJdata("shindh0429");
-    print("check 1");
-    print(Fcinform);
-    print("check 2");
-    print(Fcinform.runtimeType);
-
   }
 
   @override
@@ -27,28 +25,40 @@ class _SubscribeScreenState extends State<SubscribeScreen> {
     // const PrimaryColor = const Color(0xFFffa8a8);
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-              automaticallyImplyLeading: false, // for custom icon option setting down below
-            // title: Text(Fcinform),
-          ),
+        appBar: AppBar(
+          automaticallyImplyLeading: false, // for custom icon option setting down below
+        ),
 
         body: Container(
-            child: FutureBuilder(
+            child: FutureBuilder<DataList>(
                 future: Fcinform,
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    return Text("${snapshot}");
-                    // return Text(snapshot.data!.);
+                    // return Text(snapshot.data!.datag[1].f_name);
+                    return ListView.separated(
+                        itemBuilder:(context, index)
+                        {
+                          return
+                            ListTile(
+                              leading:ImageIcon(
+                                AssetImage("logo.png"), size: 50.0,
+                                // AssetImage(snapshot.data!.datag[index].f_logo),size: 50.0,
+                              ),
+                              title: Text(snapshot.data!.datag[index].f_name),
+                            );
+                        },
+                        separatorBuilder: (context, index) { return Divider(); },
+                        itemCount: snapshot.data!.datag.length);
+                  }
+                  else if (snapshot.hasError) {
 
-
-                  } else if (snapshot.hasError) {
-                    return Text("this is error");
+                    return Text('${snapshot.error}');
                   }
                   else {
                     return CircularProgressIndicator();
                   }})
-    ),
-    ),
+        ),
+      ),
     );
   }
 }
