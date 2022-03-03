af56e2ed9d3dfaa8b7d667c6d7b0a3db1b85ad25
chosunghyun18 <krismon@naver.com>
2022-03-03 10:03:31 +0900

    ready for pull

--- App/lib/page1/regisinput_screen.dart
+++ App/lib/page1/regisinput_screen.dart
@@ -118,31 +118,15 @@ class _RegisinputState extends State<Regisinput> {
                     hintText: 'Enter your UserName',
                     contentPadding:
                     EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(32.0)),),
+                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: TeamColor, width: 1.0), borderRadius: BorderRadius.all(Radius.circular(32.0)),),
+                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: TeamColor, width: 2.0), borderRadius: BorderRadius.all(Radius.circular(32.0)),),
                     ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: TeamColor, width: 1.0),
-                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: TeamColor, width: 2.0),
-                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    ),
-                  ),
-                  onChanged: (value) {
-                    username = value ;
-                  },
+                  onChanged: (value) {username = value ;},
                   controller: usernameInputController,
                   keyboardType: TextInputType.text,
-                  validator: (username){
-                    if (usernameValidator(username!) == null)
-                      return null;
-                    else
-                      return 'Enter a valid  username';
-                  },
-
-
+                  validator: (username)
+                  {if (usernameValidator(username!) == null) return null; else return 'Enter a valid  username';},
                 ),
                 SizedBox(
                   height: 8.0,
