62a2e338b35ab700ec10c9f1803f23c61c291d85
chosunghyun18 <krismon@naver.com>
2022-03-04 02:09:40 +0900

    add birthdaypicker in cupertinostyle

--- App/lib/page1/regisinput_screen.dart
+++ App/lib/page1/regisinput_screen.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import '../methods/toast.dart';
 import 'HomePage.dart';
 import 'package:flutter/services.dart';
 import '../constants.dart';
 import '../methods/validators.dart';
 import 'package:flutter_try/api/UserpostApi.dart';
-import 'package:flutter_try/api/currentUserservice.dart' ;
-
-
-
+import 'package:flutter_try/api/currentUserservice.dart';
 
 class Regisinput extends StatefulWidget {
   static const String id = "regisinpudt_screen";
@@ -18,7 +16,6 @@ class Regisinput extends StatefulWidget {
 }
 
 class _RegisinputState extends State<Regisinput> {
-
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   late String name;
@@ -30,214 +27,270 @@ class _RegisinputState extends State<Regisinput> {
   final String u_email = "sd";
   final String u_token = "fdfd";
 
-
-
   TextEditingController nameInputController = TextEditingController();
   TextEditingController usernameInputController = TextEditingController();
   TextEditingController phoneInputController = TextEditingController();
 
-
   bool _loading = false;
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
+  DateTime _dateTime = DateTime.now();
 
   @override
   initState() {
     super.initState();
   }
 
-  Widget _loadingWidget () {
+  Widget buildDatePicker() => SizedBox(
+        height: 120,
+        child: CupertinoDatePicker(
+          minimumYear: 1500,
+          maximumYear: 2200,
+          mode: CupertinoDatePickerMode.date,
+          initialDateTime: _dateTime,
+          onDateTimeChanged: (dateTime) =>
+              setState(() => this._dateTime = dateTime),
+        ),
+      );
+
+  static void showSheet(
+    BuildContext context, {
+    required Widget child,
+    required VoidCallback onPressed,
+  }) =>
+      showCupertinoModalPopup(
+        context: context,
+        builder: (context) => CupertinoActionSheet(
+          actions: [
+            child,
+          ],
+          cancelButton: CupertinoActionSheetAction(
+            child: Text('Done'),
+            onPressed: onPressed,
+          ),
+        ),
+      );
+
+  Widget _loadingWidget() {
     return Center(
       child: CircularProgressIndicator(),
     );
   }
+
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: Colors.white,
-      body:
-      Padding(
+      body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: _loading ? _loadingWidget() :
-        SingleChildScrollView(
-          child:Form(
-            key: _formKey,
-            child:Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(height: 52.0,),
-                Hero(
-                  tag: "logo",
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('assets/Logo.png'),
+        child: _loading
+            ? _loadingWidget()
+            : SingleChildScrollView(
+                child: Form(
+                  key: _formKey,
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        SizedBox(
+                          height: 52.0,
+                        ),
+                        Hero(
+                          tag: "logo",
+                          child: Container(
+                            height: 200.0,
+                            child: Image.asset('assets/Logo.png'),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 52.0,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter your Name',
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: TeamColor, width: 1.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: TeamColor, width: 2.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                          ),
+                          onChanged: (value) {
+                            name = value;
+                            // need to check double email ifwhen made a user in same email
+                          },
+                          controller: nameInputController,
+                          keyboardType: TextInputType.name,
+                          validator: (name) {
+                            if (nameValidator(name!) == null)
+                              return null;
+                            else
+                              return 'Enter a valid name in just english or korean';
+                          },
+                        ),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter your UserName',
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: TeamColor, width: 1.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: TeamColor, width: 2.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                          ),
+                          onChanged: (value) {
+                            username = value;
+                          },
+                          controller: usernameInputController,
+                          keyboardType: TextInputType.text,
+                          validator: (username) {
+                            if (usernameValidator(username!) == null)
+                              return null;
+                            else
+                              return 'Enter a valid  username';
+                          },
+                        ),
+                        SizedBox(
+                          height: 8.0,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter your phone',
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: TeamColor, width: 1.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: TeamColor, width: 2.0),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                          ),
+                          onChanged: (value) {
+                            phone = value;
+                            birth = value;
+                            // need to check double email ifwhen made a user in same email
+                          },
+                          controller: phoneInputController,
+                          keyboardType: TextInputType.phone,
+                          validator: (phone) {
+                            if (phoneValidator(phone!) == null)
+                              return null;
+                            else
+                              return 'Enter a valid  phone';
+                          },
+                        ),
+                        SizedBox(height: 24),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16.0),
+                          child: Material(
+                            color: TeamColor,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)),
+                            elevation: 5.0,
+                            child: MaterialButton(
+                              color: TeamColor,
+                              onPressed: () => showSheet(
+                                context,
+                                child: buildDatePicker(),
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                              child: Text(
+                                'birthday',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(vertical: 16.0),
+                          child: Material(
+                            color: TeamColor,
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(30.0)),
+                            elevation: 5.0,
+                            child: MaterialButton(
+                              onPressed: () async {
+                                if (!_formKey.currentState!.validate()) return;
+                                try {
+                                  setState(() => _loading = true);
+
+                                  createUserpost(u_token, birth, u_email, name,
+                                      phone, username);
+                                  Navigator.pushNamed(context, HomePage.id);
+                                } catch (e) {
+                                  toastError(_scaffoldKey, e);
+                                  print(e);
+                                }
+                              },
+                              minWidth: 200.0,
+                              height: 42.0,
+                              child: Text(
+                                'confirm',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 24.0,
+                        ),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: ElevatedButton(
+                            child: Text('nexttemp'),
+                            onPressed: () {
+                              Navigator.pushNamed(context, Regisinput.id);
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                SizedBox(height: 52.0,),
-
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter your Name',
-                    contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: TeamColor, width: 1.0),
-                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: TeamColor, width: 2.0),
-                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    ),
-                  ),
-                  onChanged: (value) {
-                    name = value;
-                    // need to check double email ifwhen made a user in same email
-                  },
-                  controller: nameInputController,
-                  keyboardType: TextInputType.name,
-                  validator:(name) {
-                    if (nameValidator(name!) == null)
-                      return null;
-                    else
-                      return 'Enter a valid name in just english or korean';
-                  },
-
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter your UserName',
-                    contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(32.0)),),
-                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: TeamColor, width: 1.0), borderRadius: BorderRadius.all(Radius.circular(32.0)),),
-                    focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: TeamColor, width: 2.0), borderRadius: BorderRadius.all(Radius.circular(32.0)),),
-                    ),
-                  onChanged: (value) {username = value ;},
-                  controller: usernameInputController,
-                  keyboardType: TextInputType.text,
-                  validator: (username)
-                  {if (usernameValidator(username!) == null) return null; else return 'Enter a valid  username';},
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-                TextFormField(
-                  decoration: InputDecoration
-                    (
-                    hintText: 'Enter your phone',
-                    contentPadding:
-                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: TeamColor, width: 1.0),
-                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: TeamColor, width: 2.0),
-                      borderRadius: BorderRadius.all(Radius.circular(32.0)),
-                    ),
-                  ),
-
-                  onChanged: (value) {
-                    phone = value;
-                    birth = value;
-                    // need to check double email ifwhen made a user in same email
-                  },
-                  controller: phoneInputController,
-                  keyboardType: TextInputType.phone,
-                  validator:(phone) {
-                    if (phoneValidator(phone!) == null)
-                      return null;
-                    else
-                      return 'Enter a valid  phone';
-                  },
-
-
-                ),
-                SizedBox(
-                  height: 8.0,
-                ),
-
-
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  child: Material(
-                    color: TeamColor,
-                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
-                    elevation: 5.0,
-                    child: MaterialButton(
-                      onPressed: () async{
-                        if (!_formKey.currentState!.validate()) return;
-                          try {
-                          setState(() => _loading = true);
-
-                          createUserpost(u_token,birth,u_email,name,phone,username);
-                          Navigator.pushNamed(context,HomePage.id);
-                          }
-                          catch (e)
-                          {
-                            toastError(_scaffoldKey, e);
-                            print(e);
-                          }
-                        },
-                      minWidth: 200.0,
-                      height: 42.0,
-                      child: Text(
-                        'confirm',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-
-                SizedBox(
-                  height: 24.0,
-                ),
-
-                SizedBox(
-                  width: double.infinity,
-                  height:50,
-                  child: ElevatedButton(
-                    child:Text('End'),
-                    onPressed: ()
-                    {
-                      // post to sub api
-                      Navigator.pushNamed(context,HomePage.id);
-                      },
-                  ),
-                ),
-
-
-                SizedBox(
-                  width: double.infinity,
-                  height:50,
-                  child: ElevatedButton(
-                    child:Text('nexttemp'),
-                    onPressed: (){Navigator.pushNamed(context, Regisinput.id);},
-                  ),
-                ),
-
-              ],
-            ),
-
-          ),
-          ),
-        ),
+              ),
       ),
     );
   }
-
-
 }
