04f214b3a2e149d3940200743d62b787d6a70ff9
chosunghyun18 <krismon@naver.com>
2022-03-04 13:45:02 +0900

    update style of birthday button

--- App/lib/page1/regisinput_screen.dart
+++ App/lib/page1/regisinput_screen.dart
@@ -36,7 +36,8 @@ class _RegisinputState extends State<Regisinput> {
   final _scaffoldKey = GlobalKey<ScaffoldState>();
 
   bool isDateSelected= false;
-  String birth = DateTime.now() as String ;
+  String birth = "2022-03-04";
+
   DateTime _dateTime = DateTime.now();
 
   @override
@@ -115,21 +116,11 @@ class _RegisinputState extends State<Regisinput> {
                         TextFormField(
                           decoration: InputDecoration(
                             hintText: 'Enter your Name',
-                            contentPadding: EdgeInsets.symmetric(
-                                vertical: 10.0, horizontal: 20.0),
-                            border: OutlineInputBorder(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(32.0)),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: TeamColor, width: 1.0),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(32.0)),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide:
-                                  BorderSide(color: TeamColor, width: 2.0),
+                            contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(32.0)),),
+                            enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: TeamColor, width: 1.0),
+                              borderRadius: BorderRadius.all(Radius.circular(32.0)),),
+                            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: TeamColor, width: 2.0),
                               borderRadius:
                                   BorderRadius.all(Radius.circular(32.0)),
                             ),
@@ -187,6 +178,7 @@ class _RegisinputState extends State<Regisinput> {
                         SizedBox(
                           height: 8.0,
                         ),
+
                         TextFormField(
                           decoration: InputDecoration(
                             hintText: 'Enter your phone',
@@ -225,15 +217,14 @@ class _RegisinputState extends State<Regisinput> {
                         SizedBox(height: 24),
 
  ///// get  birth day form
-                        Padding(
-                          padding: EdgeInsets.symmetric(vertical: 16.0),
-                          child: Material(
-                            color: TeamColor,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(30.0)),
-                            elevation: 5.0,
-                            child: MaterialButton(
-                              color: TeamColor,
+                        ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: TeamColor,
+                                onPrimary: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(32.0),
+                                ),
+                              ),
                               onPressed: () => showSheet(
                                 context,
                                 child: buildDatePicker(),
@@ -255,13 +246,10 @@ class _RegisinputState extends State<Regisinput> {
                                 },
                               ),
                               child: Text(
-                                'birthday : "$birth" ',
+                                'birthday : $birth ',
                                 style: TextStyle(color: Colors.white),
                               ),
                             ),
-                          ),
-                        ),
-
 
                         Padding(
                           padding: EdgeInsets.symmetric(vertical: 16.0),
