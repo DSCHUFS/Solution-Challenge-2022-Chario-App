3cb202ba473f34c184c368a1acdcb20f6d0210e9
chosunghyun18 <krismon@naver.com>
2022-03-08 10:15:13 +0900

    change ui and give info to user

--- App/lib/page1/regissub_screen.dart
+++ App/lib/page1/regissub_screen.dart
@@ -1,15 +1,12 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import '../api/Fcapi.dart';
 import 'package:flutter_try/methods/multi_select_dialog.dart';
 
+import '../api/Userapi.dart';
 import '../api/subscribePostapi.dart';
 import '../constants.dart';
 import 'HomePage.dart';
-
-
 
 class Regissub extends StatefulWidget {
   static const String id = "regissub_screen";
@@ -17,20 +14,18 @@ class Regissub extends StatefulWidget {
   _RegissubState createState() => _RegissubState();
 }
 
-
-
-class _RegissubState extends State<Regissub>
-{
+class _RegissubState extends State<Regissub> {
   late Future<FcJdata> DetailFcJdata;
   late Set<String> selectedValues;
-  List<MultiSelectDialogItem<String>> items = List.generate(7, (index) => MultiSelectDialogItem('$index+1', '미정입니다'),);
+  List<MultiSelectDialogItem<String>> items = List.generate(
+    7,
+    (index) => MultiSelectDialogItem('$index+1', '미정입니다'),
+  );
 
-  late List <String> namecheck = [];
-  late List <int> requestid = [];
+  late List<String> namecheck = [];
+  late List<int> requestid = [];
 
-  void makelist()
-  async {
-
+  void makelist() async {
     FcJdata gdata = await fetchFcJdata();
     for (int i = 0; i < 7; i++) {
       items[i].value = gdata.data[i].f_name;
@@ -42,13 +37,7 @@ class _RegissubState extends State<Regissub>
     }
     print("dfd");
     print(namecheck);
-
-
-
   }
-
-
-
 
   void submit() {
     showDialog(
@@ -74,28 +63,23 @@ class _RegissubState extends State<Regissub>
                 onPressed: () {
                   final requestpost = selectedValues.toList();
                   requestid = []; // init for re use
-                  for (int i = 0; i < requestpost.length; i++)
-                  {
-                    for (int j = 0; j < namecheck.length; j++)
-                    {
-                      if (requestpost[i] == namecheck[j])
-                      {
-                        requestid.add(j+1);
+                  for (int i = 0; i < requestpost.length; i++) {
+                    for (int j = 0; j < namecheck.length; j++) {
+                      if (requestpost[i] == namecheck[j]) {
+                        requestid.add(j + 1);
                       }
                     }
                   }
                   requestid.sort();
                   subspost().postsubs(requestid);
                   final bool re = subspost().resultc;
-                  if(re == true){
-                    Navigator.pushNamedAndRemoveUntil(context, HomePage.id, (route) => false);
+                  if (re == true) {
+                    Navigator.pushNamedAndRemoveUntil(
+                        context, HomePage.id, (route) => false);
+                  } else {
+                    print("It shoot  error in regissub scrren");
                   }
-                  else {
-                   print("It shoot  error in regissub scrren");
-                  }
-
-                }
-            ),
+                }),
           ],
         );
       },
@@ -103,10 +87,10 @@ class _RegissubState extends State<Regissub>
   }
 
   void _showMultiSelect() async {
-    selectedValues = await showDialog(context: context,
+    selectedValues = await showDialog(
+      context: context,
       builder: (context) {
-        return MultiSelectDialog
-          (
+        return MultiSelectDialog(
           title: '구독할 기관을 선택해 주세요',
           items: items,
           initialSelectedValues: selectedValues,
@@ -117,25 +101,25 @@ class _RegissubState extends State<Regissub>
     setState(() {});
   }
 
+  late Future<UData> Userform;
+
   @override
   void initState() {
     super.initState();
     DetailFcJdata = fetchFcJdata();
     selectedValues = {"초록우산어린이재단"};
     makelist();
+    Userform = fetchUserJdata();
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body:
-          Column(
-            //mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Column(
+        body: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
               children: <Widget>[
                 SizedBox(
                   height: 120.0,
@@ -150,78 +134,98 @@ class _RegissubState extends State<Regissub>
                 SizedBox(
                   height: 20.0,
                 ),
+                FutureBuilder<UData>(
+                    future: Userform,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        // Text(snapshot.data.u_Username);
+                        return Container(
+                          child: Column(
+                            children: [
+                              Text('반갑습니다!' + snapshot.data!.u_Username),
+                              SizedBox(
+                                height: 20.0,
+                              ),
 
-                Text('소식을 받아볼 기부 단채를 골라보세요!') ,
-                SizedBox(
-                  height: 40.0,
-                ),
-
-              GestureDetector(
-                onTap: _showMultiSelect,
-                child:
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20.0),
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Colors.grey[200],
-                    border: Border.all(
-                      color: Colors.black54,
-                      width: 1.0,
-                    ),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Expanded(
-                          child: Wrap(
-                            spacing: 10.0,
-                            children: selectedValues == null || selectedValues.length == 0 ? [Container()]
-                                : selectedValues.map((v) {
-                              return Chip(label: Text('$v'), labelStyle: TextStyle(color: Colors.white),
-                                backgroundColor: TeamColor,
-                                elevation: 6,
-                                onDeleted: () {
-                                  setState(() {
-                                    selectedValues.remove(v);});
-                                },
-                              );
-                            },).toList(),
+                              Text('소식을 받아볼 기부 단체를 골라보세요!'),
+                              SizedBox(
+                                height: 40.0,
+                              ),
+                            ],
                           ),
-                        ),
-                        Container(
-                          height: 60.0,
-                          child: Icon(Icons.list),
-                        ),
-                      ],
+                        );
+                      }else if (snapshot.hasError) {
+                        return Text('${snapshot.error}'+'!!this is error');}
+                      return const CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      );
+                    },
+                    ),
+                GestureDetector(
+                  onTap: _showMultiSelect,
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20.0),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      border: Border.all(
+                        color: Colors.black54,
+                        width: 1.0,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Expanded(
+                            child: Wrap(
+                              spacing: 10.0,
+                              children: selectedValues == null ||
+                                      selectedValues.length == 0
+                                  ? [Container()]
+                                  : selectedValues.map(
+                                      (v) {
+                                        return Chip(
+                                          label: Text('$v'),
+                                          labelStyle:
+                                              TextStyle(color: Colors.white),
+                                          backgroundColor: TeamColor,
+                                          elevation: 6,
+                                          onDeleted: () {
+                                            setState(() {
+                                              selectedValues.remove(v);
+                                            });
+                                          },
+                                        );
+                                      },
+                                    ).toList(),
+                            ),
+                          ),
+                          Container(
+                            height: 60.0,
+                            child: Icon(Icons.list),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-
                 SizedBox(
                   height: 30.0,
                 ),
-
-              ElevatedButton(
-                child: Text('SUBMIT'),
-                onPressed: selectedValues == null || selectedValues.length == 0 ? null
-                    : submit,
-
-              ),
-            ],
-          ),
-
-
-
-
+                ElevatedButton(
+                  child: Text('SUBMIT'),
+                  onPressed:
+                      selectedValues == null || selectedValues.length == 0
+                          ? null
+                          : submit,
+                ),
+              ],
+            ),
           ],
-
-          ),
         ),
-      );
-
+      ),
+    );
   }
 }
-
