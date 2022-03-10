2a2f3fec5fd50611a14bf7830781506664b9113c
chosunghyun18 <krismon@naver.com>
2022-03-10 19:54:00 +0900

    end mypage

--- App/lib/page1/personal_screen.dart
+++ App/lib/page1/personal_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_try/api/Userapi.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_try/page1/HomePage.dart';
+import 'package:flutter_try/page1/welcome_screen.dart';
 
 import '../constants.dart';
 
@@ -25,137 +26,134 @@ class _PersonalScreenState extends State<PersonalScreen> {
   Widget build(BuildContext context) {
     // const PrimaryColor = const Color(0xFFffa8a8);
     return Scaffold(
-      body: FutureBuilder<UData>(
-        future: Userform,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView(
-              children: <Widget>[
-                Card(
-                    child: ListTile(
-                  leading: InkWell(
-                    onTap: () {
-                      Navigator.pushNamed(context, HomePage.id);
-                    },
-                    child: Icon(Icons.arrow_back_ios),
-                  ),
-                  title: Text('My-page'),
-                  trailing: InkWell(
-                    onTap: () {
-                      Navigator.pushNamed(context, HomePage.id);
-                    },
-                    child: Icon(Icons.home),
-                  ),
-                )),
-                SizedBox(height: 30),
-                CircleAvatar(
-                  maxRadius: 100,
-                  backgroundImage: AssetImage('assets/logo_char.png'),
-                ),
-                SizedBox(height: 30),
-                // Card(
-                //   child: ListTile(
-                //     title: Text('One-line with trailing widget'),
-                //     trailing: Icon(Icons.more_vert),
-                //   ),
-                // ),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: TeamColor,
+      body:
+      SafeArea(
+        child :
+      FutureBuilder<UData>(
+            future: Userform,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Container(
+                  child: Column(
+                  children: <Widget>[
+
+                    Card(
+                        child: ListTile(
+                      leading: InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, HomePage.id);
+                        },
+                        child: Icon(Icons.arrow_back_ios),
+                      ),
+                      title: Text('My-page'),
+                      trailing: InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, HomePage.id);
+                        },
+                        child: Icon(Icons.home),
+                      ),
+                    )),
+                    SizedBox(height: 30),
+                    CircleAvatar(
+                      maxRadius: 100,
+                      backgroundImage: AssetImage('assets/logo_char.png'),
                     ),
-                  ),
-                  child: ListTile(
-                    leading: Text(
-                      "Name",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    title:
-                        Text(snapshot.data!.u_Name, textAlign: TextAlign.right),
-                    // Center(child:Text(snapshot.data!.u_Name, textAlign: TextAlign.right)),
-                    trailing: Icon(Icons.arrow_forward_ios),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: TeamColor,
-                    ),
-                  ),
-                  child: ListTile(
-                    leading: Text(
-                      "UserName",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    title: Text(snapshot.data!.u_Username,
-                        textAlign: TextAlign.right),
-                    // Center(child:Text(snapshot.data!.u_Name, textAlign: TextAlign.right)),
-                    trailing: Icon(Icons.arrow_forward_ios),
-                  ),
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: TeamColor,
-                    ),
-                  ),
-                  child: ListTile(
-                    leading: Text(
-                      "Personlal info",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                    // Center(child:Text(snapshot.data!.u_Name, textAlign: TextAlign.right)),
-                    trailing: Icon(Icons.arrow_forward_ios),
-                  ),
-                ),
-                // Container(
-                // decoration: BoxDecoration(
-                //   border: Border.all(color: TeamColor),
-                // ),
-                //   child:
-                // Card(
-                //   child: ListTile(
-                //     leading:Text("UserName",style: TextStyle(fontSize: 18),),
-                //     subtitle:Center(child:Text(snapshot.data!.u_Username)),
-                //     trailing: Icon(Icons.more_vert),),
-                //       ),
-                // ),
-                // Card(
-                //   child: ListTile(
-                //     title: Text('One-line dense ListTile'),
-                //     dense: true,
-                //   ),
-                // ),
-                // Card(
-                //   child: ListTile(
-                //     leading: FlutterLogo(size: 56.0),
-                //     title: Center(child: Text('Login - email')),
-                //     subtitle: Center(child: Text(snapshot.data!.u_Email)),
-                //     trailing: Icon(Icons.more_vert),
-                //   ),
-                // ),
-                // Card(
-                //   child: ListTile(
-                //     leading: FlutterLogo(size: 72.0),
-                //     title: Text('Three-line ListTile'),
-                //     subtitle: Text(
-                //         'A sufficiently long subtitle warrants three lines.'),
-                //     trailing: Icon(Icons.more_vert),
-                //     isThreeLine: true,
-                //   ),
-                // ),
+                    SizedBox(height: 30),
+                    // Card(
+                    //   child: ListTile(
+                    //     title: Text('One-line with trailing widget'),
+                    //     trailing: Icon(Icons.more_vert),
+                    //   ),
+                    // ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: TeamColor,
+                        ),
+                      ),
+                      child: ListTile(
+                        leading: Text(
+                          "Login-Id",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        title: Text(snapshot.data!.u_Email,
+                            textAlign: TextAlign.right),
 
 
-              ],
-            );
-          } else if (snapshot.hasError) // there will be no null info
-          {
-            return Text('${snapshot.error}' + '!!this is error');
-          }
-          return const CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-          );
-        },
+                        // trailing: Icon(Icons.arrow_forward_ios),
+
+                        // trailing: InkWell(
+                        //   onTap: () {
+                        //     Navigator.pushNamed(context, HomePage.id);
+                        //   },
+                        //   child: Icon(Icons.arrow_forward_ios),
+
+                        ),
+                      ),
+
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: TeamColor,
+                        ),
+                      ),
+                      child: ListTile(
+                        leading: Text(
+                          "UserName",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        title: Text(snapshot.data!.u_Username, textAlign: TextAlign.right),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: TeamColor,
+                        ),
+                      ),
+                      child: ListTile(
+                        leading: Text(
+                          "Personlal info",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        // Center(child:Text(snapshot.data!.u_Name, textAlign: TextAlign.right)),
+                        trailing: Icon(Icons.arrow_forward_ios),
+                      ),
+                    ),
+                    Expanded(flex: 1,child: Container(),),
+
+                    Card(
+                      elevation: 20,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Logout",
+                            style: TextStyle(fontWeight: FontWeight.bold,color: TeamColor),
+                          ),
+                          IconButton(
+                              icon: Icon(Icons.logout,color: TeamColor),
+                              onPressed: () async {
+                                await _auth.signOut();
+                                Navigator.pushNamedAndRemoveUntil(context,
+                                    WelcomeScreen.id, (route) => false);
+                              }),
+                        ],
+                      ),
+                    ),
+                  ],
+                  ),
+                );
+              } else if (snapshot.hasError) // there will be no null info
+              {
+                return Text('${snapshot.error}' + '!!this is error');
+              }
+              return const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              );
+            },
+          ),
+
       ),
     );
   }
