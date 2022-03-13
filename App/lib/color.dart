a6b58bfce02fd620fa016a72572945a75d3dedca
kimwest00 <kimwest00@hufs.ac.kr>
2022-03-14 05:01:39 +0900

    Ui 작업

--- App/lib/color.dart
+++ App/lib/color.dart
@@ -1,2 +1,10 @@
 import 'package:flutter/material.dart';
-const mainColor = const Color(0xff3EB489);
+const mainColor = const Color(0xff759D1A);
+const sdg1 = const Color(0xffE5243B);
+const sdg2 = const Color(0xffDDA83A);
+const sdg3 = const Color(0xff4C9F38);
+const sdg4 = const Color(0xffC5192D);
+const sdg6 = const Color(0xff26BDE2);
+const sdg10 = const Color(0xffDD1367);
+const sdg13 = const Color(0xff3F7E44);
+const sdg14 = const Color(0xff56C02B);
