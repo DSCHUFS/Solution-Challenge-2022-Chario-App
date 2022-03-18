24655edddeda1f20c180edb75b9715879c4f0d3f
조성현/Cho_sung_hyun <37647483+chosunghyun18@users.noreply.github.com>
2022-03-18 18:54:22 +0900

    Update subscribe_screen.dart

--- App/lib/page1/subscribe_screen.dart
+++ App/lib/page1/subscribe_screen.dart
@@ -1,69 +1,109 @@
 import 'dart:core';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_try/constants.dart';
 import '../api/Subscribeapi.dart';
+import 'package:flutter_try/detailPage/FcDetail.dart';
+import 'package:flutter_try/page1/HomePage.dart';
+import '../constants.dart';
 
-class SubscribeScreen extends StatefulWidget
-{
+class SubscribeScreen extends StatefulWidget {
   static const String id = "subscribe_screen";
 
   @override
   _SubscribeScreenState createState() => _SubscribeScreenState();
 }
 
-class _SubscribeScreenState extends State<SubscribeScreen>
-{
-  late Future <DataList> Fcinform;
+class _SubscribeScreenState extends State<SubscribeScreen> {
+  late Future<DataList> Fcinform;
   @override
   void initState()
   {
     super.initState();
-
-    //// need name
-    Fcinform  = fetchSubJdata("shindh0429");
+    Fcinform = fetchSubJdata();
   }
 
   @override
   Widget build(BuildContext context) {
     // const PrimaryColor = const Color(0xFFffa8a8);
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          automaticallyImplyLeading: false, // for custom icon option setting down below
-        ),
+    return Scaffold(
+      body: SafeArea(
+        child: FutureBuilder<DataList>(
+            future: Fcinform,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
 
-        body: Container(
-            child: FutureBuilder<DataList>(
-                future: Fcinform,
-                builder: (context, snapshot) {
+                  children: <Widget>[
+                    Card(
+                        child: ListTile(
+                          leading: InkWell(
+                            onTap: () {
+                              Navigator.pushNamed(context, HomePage.id);
+                            },
+                            child: Icon(Icons.arrow_back_ios),
+                          ),
 
-                  if (snapshot.hasData) {
-                    return  ListView.separated(
-                        itemBuilder:(context, index)
-                        {
-                          return
-                            ListTile(
-                              leading:ConstrainedBox(constraints: BoxConstraints(
-                                minWidth: 44,
-                                minHeight: 44,
-                                maxWidth: 64,
-                                maxHeight: 64, //
-                                ),child: Image.network(snapshot.data!.datag[index].f_logo, fit: BoxFit.fill),
-
+                          trailing:
+                              Wrap(
+                                spacing: 12,
+                                children:[
+                                  Text(' your like page '),
+                                  Icon(Icons.favorite, color: TeamColor,)
+                                ],
                               ),
-                                title:Center(child:Text(snapshot.data!.datag[index].f_name),)
-                            );
-                        },
-                        separatorBuilder: (context, index) { return Divider(); },
-                        itemCount: snapshot.data!.datag.length);
-                  }
-                  else if (snapshot.hasError) {
+                        )
+                    ),
 
-                    return Text('${snapshot.error}');
-                  }
-                  else {
-                    return CircularProgressIndicator();
-                  }})
-        ),
+
+                    SizedBox(height: 30),
+
+                    Expanded(
+                      child:
+                      ListView.separated(
+                        itemCount: snapshot.data!.sub_count,
+                        itemBuilder: (BuildContext context, index) {
+                          return ListTile(
+                              leading: ConstrainedBox(
+                                constraints: BoxConstraints(
+                                  minWidth: 44,
+                                  minHeight: 44,
+                                  maxWidth: 64,
+                                  maxHeight: 64, //
+                                ),
+                                child: Image.network(
+                                    snapshot.data!.sub_list[index].f_logo,
+                                    fit: BoxFit.fill),
+                              ),
+                              title: Center(
+                                child:
+                                Text(snapshot.data!.sub_list[index].f_name),
+                              ),
+                              trailing: InkWell(
+                                onTap: () {
+                                  Navigator.pushNamedAndRemoveUntil(context, NoPoverty.id, (route) => false, arguments: (snapshot.data!.sub_list[index].f_id).toString());
+                                },
+                                child:Icon(Icons.favorite , color: TeamColor),
+                              ),
+                            );
+                          },
+                        separatorBuilder: (context, index)
+                        {return Divider(color: Colors.black,);},
+                      ),
+
+                    ),
+                  ],
+                );
+              } else if (snapshot.data == null) {
+                return Center(
+                  child: Text('there is no subscribe yet'),
+                );
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              } else {
+                return CircularProgressIndicator();
+              }
+            }),
       ),
     );
   }
